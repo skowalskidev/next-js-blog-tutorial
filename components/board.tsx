6a43ca792d44d8eb74369413097eafa9300768e8
skowalskidev <skowalskidev@gmail.com>
2022-12-18 23:46:12 +0700

    Basic version of the game working

--- components/board.tsx
+++ components/board.tsx
@@ -1,53 +1,83 @@
-import React from "react";
+import React, {useState} from "react";
 import {Square} from "./square";
 
 import boardStyles from './board.module.css'
 
-export class Board extends React.Component {
-    constructor(props: any) {
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null),
-        };
-    }
-    
-    handleClick(i: any) {
-        const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+export function Board(props: any) {
+    const [state, setState] = useState({
+        squares: Array(9).fill(null),
+        xIsNext: false,
+    })
+
+    const markXOnSquares = (i: number) => {
+        const newSquares = state.squares.slice();
+        newSquares[i] = state.xIsNext ? 'X' : 'O';
+        return newSquares;
     }
 
-    renderSquare(i: any) {
-        return (
-            <Square
-                value={this.state.squares[i]}
-                onClick={() => this.handleClick(i)}
-            />
-        );
+    const handleClick = (i: number) => {
+        if (calculateWinner(state.squares) || state.squares[i]) {
+            return;
+        }
+        setState({
+            xIsNext: !state.xIsNext,
+            squares: markXOnSquares(i)
+        });
     }
 
-    render() {
-        const status = 'Next player: X';
+    const renderSquare = (i: number) => {
+        return <Square
+            value={state.squares[i]}
+            onClick={() => {
+                handleClick(i)
+            }}
+        />
+    };
 
-        return (
-            <div>
-                <div className={boardStyles.status}>{status}</div>
-                <div className={boardStyles['board-row']}>
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className={boardStyles['board-row']}>
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className={boardStyles['board-row']}>
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-            </div>
-        );
+    const winner = calculateWinner(state.squares);
+    let status;
+    if (winner) {
+        status = 'Winner: ' + winner;
+    } else {
+        status = 'Next player: ' + (state.xIsNext ? 'X' : 'O');
     }
+
+    return <div>
+        <div className={boardStyles.status}>{status}</div>
+        <div className={boardStyles['board-row']}>
+            {renderSquare(0)}
+            {renderSquare(1)}
+            {renderSquare(2)}
+        </div>
+        <div className={boardStyles['board-row']}>
+            {renderSquare(3)}
+            {renderSquare(4)}
+            {renderSquare(5)}
+        </div>
+        <div className={boardStyles['board-row']}>
+            {renderSquare(6)}
+            {renderSquare(7)}
+            {renderSquare(8)}
+        </div>
+    </div>;
+}
+
+function calculateWinner(squares: any[]) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
 }
--- components/game.tsx
+++ components/game.tsx
@@ -3,18 +3,14 @@ import {Board} from "./board";
 
 import gameStyles from './game.module.css';
 
-export class Game extends React.Component {
-    render() {
-        return (
-            <div className={gameStyles.game}>
-                <div className={gameStyles['game-board']}>
-                    <Board/>
-                </div>
-                <div className={gameStyles['game-info']}>
-                    <div>{/* status */}</div>
-                    <ol>{/* TODO */}</ol>
-                </div>
-            </div>
-        );
-    }
+export function Game() {
+    return <div className={gameStyles.game}>
+        <div className={gameStyles['game-board']}>
+            <Board/>
+        </div>
+        <div className={gameStyles['game-info']}>
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+        </div>
+    </div>
 }
